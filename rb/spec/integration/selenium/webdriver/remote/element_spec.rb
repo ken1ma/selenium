ccef9c911d7d2770e71ac38a347f853f91a52413
Alex Rodionov <p0deje@gmail.com>
2017-09-01 16:31:12 +0200

    fixup! Rework guards to use RSpec pending examples

--- rb/spec/integration/selenium/webdriver/remote/element_spec.rb
+++ rb/spec/integration/selenium/webdriver/remote/element_spec.rb
@@ -21,7 +21,7 @@ require_relative '../spec_helper'
 
 module Selenium
   module WebDriver
-    describe Element, only: {driver: :remote, browser: %i[chrome ff_esr firefox]} do
+    describe Element, only: {driver: :remote, browser: %i[chrome ff_esr]} do
       before do
         driver.file_detector = ->(_str) { __FILE__ }
       end
